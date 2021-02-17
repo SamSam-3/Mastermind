2dcfb92156a73b835f0811576d3b1b5f6693efbc
Me-k-01 <mecaman01@gmail.com>
2021-02-17 10:46:55 +0100

    exo 1

--- src/sample/Modele.java
+++ src/sample/Modele.java
@@ -1,5 +1,14 @@
 package sample;
 
+
 public class Modele {
+    enum State{IN_PROGRESS, WIN,LOOSE}
+
+    String[] colors = {};
+
+    int tentative = 10; 
+    int difficulty = 4;
+    State state = State.IN_PROGRESS;
+
     
 }
